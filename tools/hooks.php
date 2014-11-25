4aa8558ec6b9b25b4a6b260364ba30727e66d99d
Ilya Isaev <relaxart.ru@gmail.com>
2014-11-25 18:09:56 +0300

    Fix

---
+++ tools/hooks.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+echo "Installing Git Hooks"
+echo ""
+echo "This will:"
+echo "- Remove all hooks"
+echo "- Add new hooks"
+echo ""
+
+
+/*read -p "Do you wish to continue? (y/n)"
+if [ "$REPLY" != "y" ]; then
+  exit 1
+fi
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+HOOKS_PATH="$DIR/../relaxart/hooks/tools/dists"
+for entry in "$HOOKS_PATH"/*
+do
+  echo "$entry"
+done*/
